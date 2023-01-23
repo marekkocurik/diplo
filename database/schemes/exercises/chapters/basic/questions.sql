91ab7fc6916df02d1d146e1d8efbbf0e7c9a9568
marekkocurik <marek.kocurik02@gmail.com>
2023-01-23 23:25:13 +0100

    updated queries to fill data in database

--- database/schemes/exercises/chapters/basic/questions.sql
+++ database/schemes/exercises/chapters/basic/questions.sql
@@ -1,10 +1,10 @@
 INSERT INTO users.exercises(chapter_id, name, question) VALUES
-(1, 'Retrieve everything from a table', 'How can you retrieve all the information from the cd.facilities table?'),
+(1, 'Retrieve everything from a table', 'How can you retrieve all the information from the exercises.facilities table?'),
 (1, 'Retrieve specific columns from a table', 'You want to print out a list of all of the facilities and their cost to members. How would you retrieve a list of only facility names and costs?'),
 (1, 'Control which rows are retrieved', 'How can you produce a list of facilities that charge a fee to members?'),
 (1, 'Control which rows are retrieved - part 2', 'How can you produce a list of facilities that charge a fee to members, and that fee is less than 1/50th of the monthly maintenance cost? Return the facid, facility name, member cost, and monthly maintenance of the facilities in question.'),
 (1, 'Basic string searches', 'How can you produce a list of all facilities with the word "Tennis" in their name?'),
-(1, 'Matching against multiple possible values', 'How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the <c>OR</c> operator.'),
+(1, 'Matching against multiple possible values', 'How can you retrieve the details of facilities with ID 1 and 5? Try to do it without using the <em>OR</em> operator.'),
 (1, 'Classify results into buckets', 'How can you produce a list of facilities, with each labelled as "cheap" or "expensive" depending on if their monthly maintenance cost is more than $100? Return the name and monthly maintenance of the facilities in question.'),
 (1, 'Working with dates', 'How can you produce a list of members who joined after the start of September 2012? Return the memid, surname, firstname, and joindate of the members in question.'),
 (1, 'Removing duplicates, and ordering results', 'How can you produce an ordered list of the first 10 surnames in the members table? The list must not contain duplicates.'),
--- database/schemes/exercises/chapters/string/solutions.sql
+++ database/schemes/exercises/chapters/string/solutions.sql
@@ -1,8 +1,8 @@
 INSERT INTO users.solutions(exercise_id, query) VALUES
-(62, 'select surname || '', '' || firstname as name from cd.members'),
-(63, 'select * from cd.facilities where name like ''Tennis%'';'),
-(64, 'select * from cd.facilities where upper(name) like ''TENNIS%'';'),
-(65, 'select memid, telephone from cd.members where telephone ~ ''[()]'';'),
-(66, 'select lpad(cast(zipcode as char(5)),5,''0'') zip from cd.members order by zip'),
-(67, 'select substr (mems.surname,1,1) as letter, count(*) as countfrom cd.members mems group by letter order by letter'),
-(68, 'select memid, translate(telephone, ''-() '', '''') as telephone from cd.members order by memid;');
+(62, 'select surname || '', '' || firstname as name from exercises.members;'),
+(63, 'select * from exercises.facilities where name like ''Tennis%'';'),
+(64, 'select * from exercises.facilities where upper(name) like ''TENNIS%'';'),
+(65, 'select memid, telephone from exercises.members where telephone ~ ''[()]'';'),
+(66, 'select lpad(cast(zipcode as char(5)),5,''0'') zip from exercises.members order by zip;'),
+(67, 'select substr (mems.surname,1,1) as letter, count(*) as count from exercises.members mems group by letter order by letter;'),
+(68, 'select memid, translate(telephone, ''-() '', '''') as telephone from exercises.members order by memid;');
